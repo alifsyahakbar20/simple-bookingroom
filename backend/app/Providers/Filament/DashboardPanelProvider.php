3459485c3469ebcbeb891694869ee5927444fbaa
HYPE\HYPE <alifsyahakbar20@gmail.com>
2025-11-04 22:10:04 +0700

    fix:logo

--- backend/app/Providers/Filament/DashboardPanelProvider.php
+++ backend/app/Providers/Filament/DashboardPanelProvider.php
@@ -33,8 +33,9 @@ class DashboardPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#ff640a',
             ])
+            ->brandLogo(asset('images/logo.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
